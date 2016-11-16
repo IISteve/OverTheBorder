b71cb466f0b26f9a0e3d6b1fa6923921f6ce0403
IISteve <steven.braun9005@web.de>
2016-11-16 18:43:31 +0100

    Beginning

---
+++ Over The Border/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rb;
+    public float speed;
+    public float drag;
+    float msSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+        speed = 5;
+        drag = 1;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        Vector2 dragVector = new Vector2(drag, drag);
+
+        rb.AddForce(movement*speed);
+        //TODO: "drag" implementieren (Objekt wird langsamer)
+        if (msSpeed)
+        {
+            rb.AddForce(dragVector);
+        }
+    }
+}
