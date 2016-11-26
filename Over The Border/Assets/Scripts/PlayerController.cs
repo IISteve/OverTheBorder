58dfcf80bbc16c3abbeeb41e4bcd9b40b04cbb40
IISteve <steven.braun9005@web.de>
2016-11-26 12:43:59 +0100

    PowerSlider + auf x achse schiessen

--- Over The Border/Assets/Scripts/PlayerController.cs
+++ Over The Border/Assets/Scripts/PlayerController.cs
@@ -1,24 +1,31 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
     Rigidbody rb;
+    public Slider powerSlider;
     public float speed;
-    public float drag;
+    public float shootPowerMultiplier;
 
-	// Use this for initialization
-	void Start () {
+
+
+    // Use this for initialization
+    void Start () {
 
         rb = GetComponent<Rigidbody>();
         speed = 5;
-        drag = 1;
-	
-	}
+        shootPowerMultiplier = 10;
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+
+
 	}
 
     void FixedUpdate()
@@ -27,10 +34,14 @@ public class PlayerController : MonoBehaviour {
         float moveVertical = Input.GetAxisRaw("Vertical");
 
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        Vector2 dragVector = new Vector2(drag, drag);
+        Vector2 shootPower = new Vector2(powerSlider.value,0);
 
         rb.AddForce(movement*speed);
 
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rb.AddForce(shootPower*shootPowerMultiplier);
+        }
 
     }
 }
---
+++ Over The Border/Assets/Scripts/PowerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PowerController : MonoBehaviour {
+
+    public Slider powerSlider;
+    public int LaunchPower;
+    bool isfull;
+    bool isempty;
+
+    // Use this for initialization
+    void Start () {
+
+        LaunchPower = 2;
+        isfull = false;
+        isempty = true;
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (powerSlider.value == 100)
+        {
+            isfull = true;
+            isempty = false;
+        }
+        if (powerSlider.value == 0)
+        {
+            isfull = false;
+            isempty = true;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if(isfull == true)
+            {
+                powerSlider.value -= Mathf.Lerp(0f, 100f, LaunchPower * Time.deltaTime);
+            }
+            if(isempty == true)
+            {
+                powerSlider.value += Mathf.Lerp(0f, 100f, LaunchPower * Time.deltaTime);
+            }
+        }
+
+    }
+}
